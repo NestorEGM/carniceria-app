3ccc8ff39d4e7b756ceaf9c02d5dcbc96411577d
Nestor <nestor_gm02@hotmail.com>
2020-11-20 16:00:44 -0600

    create component for logo

---
+++ src/components/logo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import style from '../styles/logo.module.css';
+
+const Logo = () => {
+  // const [scrollY, setScrollY] = useState(0);
+  // useEffect(() => {
+  //   window.onscroll = () => {
+  //     const newScrollY = Math.ceil(window.scrollY / 2) * 2;
+  //     setScrollY(newScrollY);
+  //   };
+  // });
+  return (
+    <div className={style.logo} style={{
+      // top: scrollY < 100 ? -scrollY : -100,
+      // opacity: scrollY < 100 ? 1 - (scrollY / 100) : 0,
+    }}>
+      <p className={style.logo__text}>Carnicería App</p>
+    </div>
+  );
+};
+
+export default Logo;
